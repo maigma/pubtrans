d533e9ab8b758f5ff7104a4f7fff04f00cbef417
Marek Smoleň <marek.smolen@student.tuke.sk>
2018-03-07 19:34:57 +0100

    update serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from .models import AppStopsModel
 
 class AppStopsSerializer(serializers.ModelSerializer):
-
-	COERCE_DECIMAL_TO_STRING = False
+    
+    latitude = serializers.DecimalField(max_digits=8, decimal_places=6, coerce_to_string=False)
+    longitude = serializers.DecimalField(max_digits=9, decimal_places=6, coerce_to_string=False)
 
     class Meta:
         model = AppStopsModel
