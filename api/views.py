59aba5608500e36e58e4134bce2f25356b6d2b48
Marek Smoleň <marek.smolen@student.tuke.sk>
2018-03-08 18:53:56 +0100

    adress parser

--- api/views.py
+++ api/views.py
@@ -9,12 +9,18 @@ from rest_framework.decorators import api_view
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import JSONParser
 
+from urllib.request import urlopen
+import json as simplejson
+
 # Create your views here.
 
 def apina(request):
 	
     template = "api/api-view.html"
     context = {'meno' : 'ahoj'}
+
+    
+
 
 
 #    for p in Person.objects.raw('SELECT * FROM app_users'):
@@ -38,12 +44,23 @@ class AppStopsList(APIView):
         # print("latitude:", request.data["latitude"])
         # print("longitude:", request.data["longitude"])
 
+        
+
         app_stops = AppStopsModel()
-        app_stops.name = request.data["name"]
-        app_stops.address = request.data["address"]
+        
+        
         app_stops.latitude = request.data["latitude"]
         app_stops.longitude = request.data["longitude"]
         app_stops.user_id = request.data["user_id"]
+        
+
+        response = urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(app_stops.latitude)+","+str(app_stops.longitude)+"&key=AIzaSyA4mDvyVrLGN7IkOJw0ks1QACMVrZ1SQ3I")
+        data = simplejson.load(response)
+
+        formated_address = data['results'][0]["formatted_address"]
+
+        app_stops.name = formated_address
+        app_stops.address = formated_address
         app_stops.save()
 
 
