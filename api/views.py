da9df82abc88d10f6cdf46835fb4f3bf87ad00db
Marek Smoleň <marek.smolen@student.tuke.sk>
2018-03-08 16:39:11 +0100

    post request

--- api/views.py
+++ api/views.py
@@ -4,6 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import AppStopsSerializer
 
+# rest framework for post request
+from rest_framework.decorators import api_view
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import JSONParser
+
 # Create your views here.
 
 def apina(request):
@@ -17,7 +22,6 @@ def apina(request):
 
     return render(request, template, {'meno': 'ahoj'})
 
-
 class AppStopsList(APIView):
 
     def get(self, request):
@@ -25,5 +29,14 @@ class AppStopsList(APIView):
         serializer = AppStopsSerializer(stops, many=True)
         return Response(serializer.data)
 
-    def post(self):
-        pass 
+    # @csrf_protect
+    @parser_classes((JSONParser,))
+    def post(self, request, format=None):
+
+
+        print(request.data)
+        print("id:", request.data["id"])
+        print("latitude:", request.data["latitude"])
+        print("longitude:", request.data["longitude"])
+
+        return Response({'received data': request.data})
